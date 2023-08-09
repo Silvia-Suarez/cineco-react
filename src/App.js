ef8646eda5a3c88ef25254421996397f7cb86bba
Daniel Garzon <dgarzonvill@gmail.com>
2023-08-08 23:37:29 -0500

    Amplify

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+
+
 class App extends React.Component {
   render() {
     return (
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,39 +1,20 @@
-import React, { useState, useContext } from "react";
-import MainStructure from "../components/MainStructure";
-import Input from "../components/Inputs/Input";
-import PasswordInput from "../components/Inputs/PasswordInput";
-import { SecondaryButton } from "../components/Buttons";
+import { Amplify } from "aws-amplify";
 
-export default function LoginPage({}) {
-  const [email, SetEmail] = useState("");
-  const [password, SetPassword] = useState("");
+ 
 
-  return (
-    <>
-      <MainStructure showCarrouselTop={false} showCarrouselBot={false}>
-        <section className="flex justify-center py-20 w-full">
-          <div className="rounded-3xl w-96 flex flex-col shadow-lg py-10 px-10">
-            <h1 className="text-6xl">Login</h1>
-            <form className="flex-col pt-5 flex space-y-4">
-              <div>
-                <Input
-                  value={email}
-                  onChangeInput={SetEmail}
-                  placeholder={"Ingrese su correo"}
-                ></Input>
-              </div>
-              <div>
-                <PasswordInput
-                  value={password}
-                  onChangeInput={SetPassword}
-                  placeholder={"Ingrese su contraseña"}
-                ></PasswordInput>
-              </div>
-              <SecondaryButton>Ingresar</SecondaryButton>
-            </form>
-          </div>
-        </section>
-      </MainStructure>
-    </>
-  );
+import { Authenticator } from "@aws-amplify/ui-react";
+
+import "@aws-amplify/ui-react/styles.css";
+
+ 
+
+import awsExports from "../aws-exports";
+
+Amplify.configure(awsExports);
+
+ 
+
+export default function LoginPage() {
+
+  return <Authenticator>{({ signOut, user }) => null}</Authenticator>;
 }
