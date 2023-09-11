3a6b366b76a0271032e15b2624a899a3b1fb12e4
Silvia-Suarez <silviznatalia@gmail.com>
2023-09-11 15:10:49 -0500

    correcion final

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,10 +3,8 @@ import { Auth } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
-  const navigate = useNavigate();
   useEffect(() => {
     Auth.federatedSignIn();
   }, []);
